0cb389d8e148035b70fb68830804fad98bfc216e
rhoblack <rhoblack@gmail.com>
2022-08-31 08:14:40 +0000

    main 수정

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main()
         distance = ultraSonic.readDistance();
         std::cout << "distance = " << distance << "cm" <<std::endl;
 
-        delay(1000);
+        delay(800);
     }
 
     return 0;
